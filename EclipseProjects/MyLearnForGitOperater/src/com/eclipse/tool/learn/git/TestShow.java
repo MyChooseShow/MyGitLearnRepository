d6ee86bb25e8c8bf4415e66b3485ba9469358e0f
MyChooseShow <mymvyuncount@126.com>
2017-01-03 15:36:11 +0800

    Update TestShow.java

--- EclipseProjects/MyLearnForGitOperater/src/com/eclipse/tool/learn/git/TestShow.java
+++ EclipseProjects/MyLearnForGitOperater/src/com/eclipse/tool/learn/git/TestShow.java
@@ -3,5 +3,11 @@ package com.eclipse.tool.learn.git;
 public class TestShow {
 	{
 		System.out.println("Create on Service");
+	
+		//2017-01-03 change on service
+	}
+	
+	public static void main(Strings[] args) {
+		System.out.println("Println Test");
 	}
 }
